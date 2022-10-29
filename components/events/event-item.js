33070338440b8217e53ad4cc777e880306405428
birkan <b.561birkan@gmail.com>
2022-10-29 22:35:00 +0300

    adding icons to next.js project

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,8 @@
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
-
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 const EventItem = ({ event }) => {
   const { title, image, date, location, id } = event;
 
@@ -21,14 +23,21 @@ const EventItem = ({ event }) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
