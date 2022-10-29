4d3d64a2452f1be234f917392e97b3284f092400
birkan <b.561birkan@gmail.com>
2022-10-29 21:50:00 +0300

    styling components in Next.js

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import classes from "./event-item.module.css";
 
 const EventItem = ({ event }) => {
   const { title, image, date, location, id } = event;
@@ -14,19 +15,21 @@ const EventItem = ({ event }) => {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={image} alt={title} />
-      <div>
-        <h2>{title}</h2>
-        <div>
-          <time>{humanReadableDate}</time>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
+          </div>
         </div>
-        <div>
-          <address>{formattedAddress}</address>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore event</Link>
         </div>
-      </div>
-      <div>
-        <Link href={exploreLink}>Explore event</Link>
       </div>
     </li>
   );
