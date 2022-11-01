76da1e469fe754c489d6198751f533ea7b4931f2
birkan <b.561birkan@gmail.com>
2022-11-01 16:14:17 +0300

    a form for filtering events, not has logic yet

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,10 +2,17 @@ import Link from "next/link";
 import classes from "./button.module.css";
 
 const Button = (props) => {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <button className={classes.btn}>{props.children}</button>
+      </Link>
+    );
+  }
   return (
-    <Link href={props.link}>
-      <button className={classes.btn}>{props.children}</button>
-    </Link>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 };
 export default Button;
