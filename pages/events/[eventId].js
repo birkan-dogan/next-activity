d91fc979e24234ae9707fb7e928ac28c61a89dd2
birkan <b.561birkan@gmail.com>
2022-11-01 15:01:49 +0300

    taking id from url, fetching specific data & comp.

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,27 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../data/dummy-data";
+
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 const EventDetailPage = () => {
+  // get event by id
+  const router = useRouter();
+  const id = router.query.eventId;
+
+  const event = getEventById(id);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
   return (
     <div>
-      <h1>single event</h1>
+      <EventSummary title={event.title} />
+      <EventLogistics event={event} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
     </div>
   );
 };
