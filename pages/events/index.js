7d4849c9dec026ce4e22e16e8b3602a1e46ece32
birkan <b.561birkan@gmail.com>
2022-11-01 16:27:15 +0300

    navigating [...slug] page programmatically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
 import { getAllEvents } from "../../data/dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
+import { useRouter } from "next/router";
 
 const AllEventsPage = () => {
   const events = getAllEvents();
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    const fullpath = `/events/${year}/${month}`; // the slug route will be triggered
+    router.push(fullpath);
+  };
 
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch search={findEventsHandler} />
       <EventList items={events} />
     </div>
   );
