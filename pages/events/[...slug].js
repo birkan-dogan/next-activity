0de81984a96422e3a552ee3da819743bbd2969b4
birkan <b.561birkan@gmail.com>
2022-11-01 17:46:31 +0300

    extracting data on the catch-all page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,36 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../data/dummy-data";
+
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = +filterData[0];
+  const filteredMonth = +filterData[1];
+
+  if (
+    isNaN(filteredYear) ||
+    isNaN(filteredMonth) ||
+    filteredMonth < 1 ||
+    filteredMonth > 12
+  ) {
+    return <p>Invalid filter, please adjust your filters</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: filteredYear,
+    month: filteredMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>filtered events</h1>
