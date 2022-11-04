2710768b81c5046e5217245036b8aebfe8c52199
birkan <b.561birkan@gmail.com>
2022-11-04 18:37:31 +0300

    pre-rendering on events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,11 @@
-import { getAllEvents } from "../../data/dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { useRouter } from "next/router";
 
-const AllEventsPage = () => {
-  const events = getAllEvents();
+const AllEventsPage = (props) => {
+  // const events = getAllEvents();
+  const { events } = props;
   const router = useRouter();
 
   const findEventsHandler = (year, month) => {
@@ -20,3 +21,12 @@ const AllEventsPage = () => {
   );
 };
 export default AllEventsPage;
+
+export const getStaticProps = async () => {
+  const events = await getAllEvents();
+
+  return {
+    props: { events: events },
+    revalidate: 1800,
+  };
+};
