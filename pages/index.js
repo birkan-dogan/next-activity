5d383cbcfbcd94f435002527af9dd015f2a00753
birkan <b.561birkan@gmail.com>
2022-11-04 15:40:14 +0300

    pre-rendering page by using static generation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import { getFeaturedEvents } from "../data/dummy-data";
 import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
+export default function Home(props) {
+  // const featuredEvents = getFeaturedEvents();
+  const { featuredEvents } = props;
   return (
     <div>
       <Head>
@@ -15,3 +16,15 @@ export default function Home() {
     </div>
   );
 }
+
+// using getStaticProps function makes sense here, we don't need to pre-render it for every request
+
+export const getStaticProps = async () => {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents: featuredEvents,
+    },
+  };
+};
