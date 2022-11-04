24b835a607bdd9a2371c1415deafbe5e7c54a878
birkan <b.561birkan@gmail.com>
2022-11-04 17:48:32 +0300

    static generation for eventId page which depends on specific id

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,16 +1,19 @@
-import { useRouter } from "next/router";
-import { getEventById } from "../../data/dummy-data";
+// import { useRouter } from "next/router";
 
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 
-const EventDetailPage = () => {
-  // get event by id
-  const router = useRouter();
-  const id = router.query.eventId;
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 
-  const event = getEventById(id);
+const EventDetailPage = (props) => {
+  // get event by id
+  // const router = useRouter();
+  // const id = router.query.eventId;
+
+  const { selectedEvent } = props;
+
+  const event = selectedEvent;
 
   if (!event) {
     return <p>No event found!</p>;
@@ -27,3 +30,33 @@ const EventDetailPage = () => {
 };
 
 export default EventDetailPage;
+
+// we definitely want to have some data on single event page right from the start because it has all the details for an event
+
+// data on this page doesn't change all the time, so pre-generating sounds good
+
+export const getStaticProps = async (context) => {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+};
+
+/*
+this is a dynamic page and there is an infinite amount of possible pages that could be generated, Next.js doesn't know which eventId it should be pre-generated. That's why we need another function to tell Next.js which eventIds we have.
+*/
+
+export const getStaticPaths = async () => {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  return {
+    paths: paths,
+    fallback: false,
+  };
+};
