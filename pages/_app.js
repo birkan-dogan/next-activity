7d6d7b10278125e5d6bfa22bcd0fc836ab90093c
birkan <b.561birkan@gmail.com>
2022-11-05 17:32:11 +0300

    application wide settings on _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0,  width=device-width"
+        />
+        {/* this meta tag is often added to pages to ensure that the page is responsive and scales correctly and we threfore typically want to add this to all our pages. */}
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
 }
 
 export default MyApp;
+
+/*
+Besides reusing our Head content inside of a component, we also might have certain Head settings that should be the same across all page components. So, we don't want to copy and paste it into every single page component. Setting it up once in _app.js file to apply to all the pages.
+*/
