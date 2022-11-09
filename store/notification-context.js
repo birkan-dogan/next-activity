79c01a122541646f90f1a0977820bb3f7e542ea8
birkan <b.561birkan@gmail.com>
2022-11-09 23:48:54 +0300

    notification works depending on status and display

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NotificationContext = createContext({
   notification: null,
@@ -8,6 +8,21 @@ const NotificationContext = createContext({
 
 export const NotificationContextProvider = ({ children }) => {
   const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
 
   const showNotificationHandler = (notificationData) => {
     // we expect that we get information about the notification that should be shown as a parameter here.
